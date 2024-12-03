74353a320aa3c46852e827ca50442f5d2ceadffb
Johannes Rabausch <mail@jrabausch.de>
2024-12-03 23:12:50 +0100

    test: added more tests

--- spec/radbas-container_spec.cr
+++ spec/radbas-container_spec.cr
@@ -3,9 +3,30 @@ require "./spec_helper"
 describe Radbas::Container do
   describe "#get" do
     it "can get self" do
-      container = Container.new
+      container = TestContainer.new
       entry = container.get(Radbas::Container)
       entry.should eq container
+    end
+
+    it "resolves registered services" do
+      container = TestContainer.new
+      service = container.get(TestService)
+      service.should be_a TestService
+      service.priv.should be_a PrivateService
+    end
+
+    it "should throw on subclass get" do
+      expect_raises Radbas::Container::SubClassAccessException do
+        container = TestContainer.new
+        container.get(SubService)
+      end
+    end
+
+    it "should throw on circular reference" do
+      expect_raises Radbas::Container::CircularReferenceException do
+        container = TestContainer.new
+        container.get(CircularService)
+      end
     end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,5 +1,30 @@
 require "spec"
 require "../src/radbas-container"
 
-class Container < Radbas::Container
+class TestService
+  getter priv
+
+  def initialize(
+    @priv : PrivateService,
+    @auto : TestModule::AutoService
+  ); end
+end
+
+class SubService < TestService; end
+
+class PrivateService; end
+
+module TestModule
+  class AutoService; end
+end
+
+class CircularService; end
+
+class TestContainer < Radbas::Container
+  autowire(TestModule)
+  register(TestService, public: true)
+  register(PrivateService)
+  register(CircularService, factory: ->{
+    get(CircularService)
+  }, public: true)
 end
